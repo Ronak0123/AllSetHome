50b449781010f46044053c94557f2b3232cd4f1a
Ronak <ronakgopani.rg@gmail.com>
2021-05-08 22:15:40 +0530

    1.0

---
+++ app/src/main/java/com/ronak/allsethome/ui/home/Detail_screen.kt
@@ -0,0 +1,12 @@
+package com.ronak.allsethome.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ronak.allsethome.R
+
+class Detail_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_screen)
+    }
+}
