14fecaef4334e08ab44d991f9f9691000d515b4c
Ronak <ronakgopani.rg@gmail.com>
2021-05-10 21:48:24 +0530

    1.0

---
+++ app/src/main/java/com/ronak/allsethome/ui/home/My_Bookings.kt
@@ -0,0 +1,12 @@
+package com.ronak.allsethome.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ronak.allsethome.R
+
+class My_Bookings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_bookings)
+    }
+}
